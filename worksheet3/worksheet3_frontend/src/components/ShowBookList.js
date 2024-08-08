9b7b6760027a4fe24c1968e5e9be4a43cf98333a
PineappleBearr <mrbear030@gmail.com>
2024-08-08 14:14:46 +1200

    Fixed issues

--- worksheet3/worksheet3_frontend/src/components/ShowBookList.js
+++ worksheet3/worksheet3_frontend/src/components/ShowBookList.js
@@ -1,1 +1,23 @@
+import React from 'react';
+
+const ShowBookList = () => {
+    // Sample data for books
+    const books = [
+        { id: 1, title: 'The Great Gatsby' },
+        { id: 2, title: 'To Kill a Mockingbird' },
+        { id: 3, title: '1984' }
+    ];
+
+    return (
+        <div>
+            <h1>Book List</h1>
+            <ul>
+                {books.map(book => (
+                    <li key={book.id}>{book.title}</li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
 export default ShowBookList;
