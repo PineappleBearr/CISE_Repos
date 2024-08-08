560f81a247c764d941251b30480cac4829f2a7c2
PineappleBearr <mrbear030@gmail.com>
2024-08-08 14:29:59 +1200

    Remove worksheet3, restart

--- worksheet3/worksheet3_frontend/pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-// pages/_app.tsx
-import '../src/app/globals.css'
-import type { AppProps } from 'next/app'
-
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
-
-export default MyApp
